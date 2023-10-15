b3c663e0f70852c7a1ca83b8cf52854808adaff2
Tine Kondo <kondotine@gmail.com>
2023-10-16 00:46:57 +0200

    chore: fix `vite` and `vitest` configurations

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,9 +1,16 @@
-/// <reference types="vitest" />
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import MarkdownHMR from './plugins/hmr-markdown';
 
 export default defineConfig({
+  test: {
+    globals: true,
+    cache: {
+      dir: '../node_modules/.vitest',
+    },
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+  },
   cacheDir: '../node_modules/.vite/docs',
 
   server: {
